b80348660ee66e28fab375bc27632280d2bb714d
Matías Martearena <maticorvooficial@gmail.com>
2023-07-26 13:29:17 -0300

    feat(register page): add register page with styles and html validations

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
-const PORT = 3000
+const path = require('path')
 const publicPath = path.resolve(__dirname, './public')
 
 app.use(express.static(publicPath))
-app.listen(PORT, () => console.log(`Servidor en PORT:${PORT} corriendo`))
+
 app.get('/', (req, res) => {
    res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
+
+app.get('/register', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './views/register.html'))
+})
+
+const PORT = 3000
+app.listen(PORT, () => console.log(`Servidor en PORT:${PORT} corriendo`))
