5c5d5ef39ee4ac9bb174e1294d1072e9f2f59fe1
Matías Martearena <maticorvooficial@gmail.com>
2023-07-28 19:08:53 -0300

    feat(home, login, register): add login page, css of login page, media queries of login page, fix home a tags, fix seach bar of home page and fix register page button

--- app.js
+++ app.js
@@ -13,5 +13,17 @@ app.get('/register', (req, res) => {
    res.sendFile(path.resolve(__dirname, './views/register.html'))
 })
 
+app.post('/register', (req, res) => {
+   res.redirect('/')
+})
+
+app.get('/login', (req, res) => {
+   res.sendFile(path.resolve(__dirname, './views/login.html'))
+})
+
+app.post('/login', (req, res) => {
+   res.redirect('/')
+})
+
 const PORT = 3000
 app.listen(PORT, () => console.log(`Servidor en PORT:${PORT} corriendo`))
